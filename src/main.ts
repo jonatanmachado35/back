7d9d3e3153a582aebb1a510bc48b7e475e361d7c
Jonatan Machado <jonatanmachado35@gmail.com>
2025-11-26 19:16:41 -0300

    socker real time

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true, // reflect request origin to allow cross-origin calls
-    credentials: true,
+    origin: '*',
   });
 
   app.useGlobalPipes(
